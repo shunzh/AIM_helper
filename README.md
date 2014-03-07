fc091cb5e049acea4a7332be143d5c9acb44c094
Shun Zhang <menie482@gmail.com>
2014-03-06 23:22:58 -0600

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-intersection_analyzer
+intersection analyzer
 =====================
 
 helper repo for AIM
+
+Assumption:
+- Vehicle deceleration model is fixed.
+- Vehicle going straight.
+- No real simulation.
+
+Goal
+- Find optimal solution for some scenarios.
+- Compare FCFS, FCFS-batch and RL.
---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Main file for intersection simulator.
+No grid world simulation done. Taken as points.
+"""
+
+def init(laneNum, spawnRateSpec = None):
+	"""
+	Ini
+	"""
+
+def step():
+
+def log():
+
+def main():
+	init()
+
+	t = 0
+	while t < MAX_TIME:
+		step()
+
+	log()
+
+if __name__ == "__main__":
+	main()
